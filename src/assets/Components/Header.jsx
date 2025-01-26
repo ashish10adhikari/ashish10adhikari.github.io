1a9622bf385fa420741841b37133876cc75dcacb
AshishAd <ashish10adhikari@gmail.com>
2025-01-26 16:47:46 +0545

    added pic & text in header

--- src/assets/Components/Header.jsx
+++ src/assets/Components/Header.jsx
@@ -3,8 +3,17 @@ import profileImg from "../images/profile.jpg";
 
 const Header = () => {
   return (
-    <div id="#Header">
-      <h1>hello</h1>
+    <div id="#Header" className="min-h-screen flex items-center justify-center pt-20">
+      <div className="flex justify-evenly container mx-auto">
+        <div className=" flex flex-col items-center justify-center">
+          <h3 className="text-2xl">Hi, I am</h3>
+          <h2 className="text-4xl font-bold">Ashish Adhikari</h2>
+          <h1 className="text-5xl font-bold">Software Developer</h1>
+        </div>
+        <div>
+          <img src={profileImg} alt="" className="h-50 rounded-full w-full max-w-lg"/>
+        </div>
+      </div>
       
     </div>
   );
--- src/assets/Components/Navbar.jsx
+++ src/assets/Components/Navbar.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <div className="flex justify-evenly container py-5 px-20 mx-auto shadow-md gap-20">
+    <div className="flex justify-evenly container py-5 px-20 mx-auto shadow-md gap-20 fixed z-10">
       <div className="justify-start">
-        <h1 className="text-4xl font-bold">Ashish.</h1>
+        <a href="#Header"><h1 className="text-4xl font-bold">Ashish.</h1></a>
       </div>
       <ul className="flex gap-8 px-5 py-3 font-bold">
         <a className="hover:text-blue-500 transition-all" href="#Header">HOME</a>
